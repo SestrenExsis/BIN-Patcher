2a28cf59e8fc844fd7ba90b11299e8ee82841b5d
SestrenExsis <sestrenexsis@gmail.com>
2026-03-31 04:52:47 -0500

    Pass BIN location as an argument

--- extract.bat
+++ extract.bat
@@ -1,16 +1,18 @@
+set BIN=%1
+
 rm -f build/extraction-template.json
 rm -f build/extraction.json
 
 python tools/yaml-to-json.py "data/aliases.yaml" "build/aliases.json"
 
 python tools/generate-extraction-template.py "data/extraction-template.yaml" "build/extraction-template.json"
-node sotn extract -b "build/Castlevania - Symphony of the Night (Track 1).bin" -t "build/extraction-template.json" -e "build/extraction.json"
+node sotn extract -b %BIN% -t "build/extraction-template.json" -e "build/extraction.json"
 
 python tools/generate-extraction-template.py "data/extraction-template.yaml" "build/extraction-template.json" --previous "build/extraction.json"
-node sotn extract -b "build/Castlevania - Symphony of the Night (Track 1).bin" -t "build/extraction-template.json" -e "build/extraction.json"
+node sotn extract -b %BIN% -t "build/extraction-template.json" -e "build/extraction.json"
 
 python tools/generate-extraction-template.py "data/extraction-template.yaml" "build/extraction-template.json" --previous "build/extraction.json"
-node sotn extract -b "build/Castlevania - Symphony of the Night (Track 1).bin" -t "build/extraction-template.json" -e "build/extraction.json"
+node sotn extract -b %BIN% -t "build/extraction-template.json" -e "build/extraction.json"
 
 python tools/generate-extraction-template.py "data/extraction-template.yaml" "build/extraction-template.json" --previous "build/extraction.json"
-node sotn extract -b "build/Castlevania - Symphony of the Night (Track 1).bin" -t "build/extraction-template.json" -e "build/extraction.json"
+node sotn extract -b %BIN% -t "build/extraction-template.json" -e "build/extraction.json"
