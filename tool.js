123469b3690f3fdb157f8a0936e139a07b7bab02
SestrenExsis <sestrenexsis@gmail.com>
2026-06-26 05:29:35 -0500

    Search for and parse cutscene data

--- tool.js
+++ tool.js
@@ -1,7 +1,7 @@
 import yargs from 'yargs'
 import fs from 'fs'
 import crypto from 'crypto'
-import { Address, GameData, toHex } from './src/common.js'
+import { Address, GameData, toHex, toVal } from './src/common.js'
 
 const argv = yargs(process.argv.slice(2))
     .command({ // alias
@@ -246,6 +246,208 @@ const argv = yargs(process.argv.slice(2))
                     }
                 })
             });
+        }
+    })
+    .command({ // cutscene
+        command: 'cutscene',
+        describe: 'Search for cutscene data on a PS1 binary',
+        builder: (yargs) => {
+            return yargs
+            .option('bin', {
+                alias: 'b',
+                describe: 'Binary file to extract data from',
+                type: 'string',
+                normalize: true,
+            })
+            .option('start', {
+                alias: 's',
+                describe: 'Starting index in GAMEDATA',
+                type: 'number',
+            })
+            .option('length', {
+                alias: 'l',
+                describe: 'Number of bytes in GAMEDATA to search',
+                type: 'number',
+            })
+            .demandOption(['bin'])
+        },
+        handler: (argv) => {
+            const binFile = fs.openSync(argv.bin, 'r')
+            const binFileSize = fs.fstatSync(binFile).size
+            const buffer = Buffer.alloc(binFileSize)
+            fs.readSync(binFile, buffer, 0, binFileSize)
+            fs.closeSync(binFile)
+            const digest = crypto.createHash('sha256').update(buffer).digest()
+            console.log('Digest of disc image', digest.toString('hex'))
+            const bin = new GameData(buffer, toVal(argv.start ?? 0))
+            const preamble = '10 00'.split(' ').map((hexString) => { return Number.parseInt(hexString, 16)})
+            console.log('preamble:', preamble)
+            for (let offset = 0; offset < (argv.length ?? 0x100); offset += 1) {
+                bin.set((argv.start ?? 0) + offset)
+                let validCutsceneInd = true
+                for (let matchCount = 0; matchCount < preamble.length; matchCount++) {
+                    const byte = bin.read('u8')
+                    if (byte != preamble[matchCount])  {
+                        validCutsceneInd = false
+                        break
+                    }
+                }
+                if (!validCutsceneInd) {
+                    continue
+                }
+                bin.set((argv.start ?? 0) + offset)
+                validCutsceneInd = true
+                let maxTextLength = 0
+                const script = []
+                const currentString = []
+                let args = []
+                while (validCutsceneInd) {
+                    const byte = bin.read('u8')
+                    if (byte >= 0x00 && byte <= 0x18) {
+                        if (currentString.length > 0) {
+                            maxTextLength = Math.max(maxTextLength, currentString.length)
+                            script.push(`text(${currentString.join('')})`)
+                            while (currentString.length > 0) {
+                                currentString.pop()
+                            }
+                        }
+                        switch (byte) {
+                            case 0x00:
+                                script.push('end')
+                                break
+                            case 0x01:
+                                script.push('line')
+                                break
+                            case 0x02:
+                                args.push(bin.read('u8'))
+                                script.push(`delay(${args.join(', ')})`)
+                                break
+                            case 0x03:
+                                args.push(bin.read('u8'))
+                                script.push(`pause(${args.join(', ')})`)
+                                break
+                            case 0x04:
+                                script.push('hideDialog')
+                                break
+                            case 0x05:
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                script.push(`setPortrait(${args.join(', ')})`)
+                                break
+                            case 0x06:
+                                script.push('nextDialog')
+                                break
+                            case 0x07:
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                script.push(`setPosition(${args.join(', ')})`)
+                                break
+                            case 0x08:
+                                script.push('closeDialog')
+                                break
+                            case 0x09:
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                script.push(`playSound(${args.join(', ')})`)
+                                break
+                            case 0x0A:
+                                script.push('waitForSound')
+                                break
+                            case 0x0B:
+                                script.push('unknown0B')
+                                break
+                            case 0x0C:
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                script.push(`setEnd(${args.join(', ')})`)
+                                break
+                            case 0x0D:
+                                script.push('unknown0D')
+                                break
+                            case 0x0E:
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                script.push(`unknown0E(${args.join(', ')})`)
+                                break
+                            case 0x0F:
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                script.push(`unknown0F(${args.join(', ')})`)
+                                break
+                            case 0x10:
+                                args.push(bin.read('u8'))
+                                script.push(`waitForFlag(${args.join(', ')})`)
+                                break
+                            case 0x11:
+                                args.push(bin.read('u8'))
+                                script.push(`setFlag(${args.join(', ')})`)
+                                break
+                            case 0x12:
+                                script.push('unknown12')
+                                break
+                            case 0x13:
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                script.push(`loadPortrait(${args.join(', ')})`)
+                                break
+                            case 0x14:
+                                args.push(bin.read('u8'))
+                                args.push(bin.read('u8'))
+                                script.push(`unknown14(${args.join(', ')})`)
+                                break
+                            case 0x15:
+                                script.push('unknown15')
+                                break
+                            case 0x16:
+                                args.push(bin.read('u8'))
+                                script.push(`unknown16(${args.join(', ')})`)
+                                break
+                            case 0x17:
+                                script.push('unknown17')
+                                break
+                            case 0x18:
+                                args.push(bin.read('u8'))
+                                script.push(`waitForFlagReset(${args.join(', ')})`)
+                                break
+                            default:
+                                break
+                        }
+                        while (args.length > 0) {
+                            args.pop()
+                        }
+                    }
+                    else if (byte >= 0x20 && byte <= 0x7e) {
+                        currentString.push(String.fromCharCode(byte))
+                    }
+                    else {
+                        validCutsceneInd = false
+                    }
+                    if (script.length > 0 && script.at(-1) === 'end') {
+                        break
+                    }
+                }
+                if (validCutsceneInd && script.length > 20 && maxTextLength >= 3) {
+                    const address = new Address('GAMEDATA', argv.start ?? 0, offset)
+                    console.log('game:', toHex(address.gameDataAddress, 8), 'disc:', toHex(address.toDiscAddress(), 8))
+                    script.forEach((line, index) => {
+                        console.log('  ', index, line)
+                    })
+                }
+                maxTextLength = 0
+            }
         }
     })
     .demandCommand(1)
